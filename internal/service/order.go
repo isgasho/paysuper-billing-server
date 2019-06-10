f7476abd4db073a53865dd94303f26ebaeafce49
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-06-10 16:15:44 +0300

    PAY-0 return card holder name by saved cards

--- internal/service/order.go
+++ internal/service/order.go
@@ -437,6 +437,7 @@ func (s *Service) PaymentFormJsonDataProcess(
 
 	rsp.Id = order.Uuid
 	rsp.Account = order.ProjectAccount
+	rsp.Description = order.Description
 	rsp.HasVat = order.Tax.Amount > 0
 	rsp.Vat = order.Tax.Amount
 	rsp.Currency = order.ProjectIncomeCurrency.CodeA3
@@ -891,6 +892,7 @@ func (s *Service) saveRecurringCard(order *billing.Order, recurringId string) {
 		ProjectId:  order.Project.Id,
 		MerchantId: order.Project.MerchantId,
 		MaskedPan:  order.PaymentMethodTxnParams[pkg.PaymentCreateFieldPan],
+		CardHolder: order.PaymentMethodTxnParams[pkg.PaymentCreateFieldHolder],
 		Expire: &entity.CardExpire{
 			Month: order.PaymentRequisites[pkg.PaymentCreateFieldMonth],
 			Year:  order.PaymentRequisites[pkg.PaymentCreateFieldYear],
@@ -1613,9 +1615,10 @@ func (v *PaymentFormProcessor) processPaymentMethodsData(pm *billing.PaymentForm
 
 			for _, v := range rsp.SavedCards {
 				d := &billing.SavedCard{
-					Id:     v.Id,
-					Pan:    v.MaskedPan,
-					Expire: &billing.CardExpire{Month: v.Expire.Month, Year: v.Expire.Year},
+					Id:         v.Id,
+					Pan:        v.MaskedPan,
+					CardHolder: v.CardHolder,
+					Expire:     &billing.CardExpire{Month: v.Expire.Month, Year: v.Expire.Year},
 				}
 
 				pm.SavedCards = append(pm.SavedCards, d)
@@ -1768,6 +1771,7 @@ func (v *PaymentCreateProcessor) processPaymentFormData() error {
 			order.PaymentRequisites[pkg.PaymentCreateFieldPan] = storedCard.MaskedPan
 			order.PaymentRequisites[pkg.PaymentCreateFieldMonth] = storedCard.Expire.Month
 			order.PaymentRequisites[pkg.PaymentCreateFieldYear] = storedCard.Expire.Year
+			order.PaymentRequisites[pkg.PaymentCreateFieldHolder] = storedCard.CardHolder
 			order.PaymentRequisites[pkg.PaymentCreateFieldRecurringId] = storedCard.RecurringId
 		} else {
 			validator := &bankCardValidator{
