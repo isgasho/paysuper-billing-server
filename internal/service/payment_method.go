5c1610a339109905677a9bb5aa423b7523ee401b
Vadim Sabirov <pr0head@gmail.com>
2019-06-14 07:55:18 +0300

    PAY-208 Add methods for CRUD production settings for the payment method

--- internal/service/payment_method.go
+++ internal/service/payment_method.go
@@ -17,8 +17,14 @@ const (
 
 	collectionPaymentMethod = "payment_method"
 
-	paymentMethodErrorName          = "payment method must contain name"
-	paymentMethodErrorPaymentSystem = "payment method must contain of payment system"
+	paymentMethodErrorName                       = "payment method must contain name"
+	paymentMethodErrorPaymentSystem              = "payment method must contain of payment system"
+	paymentMethodErrorIncorrectCurrencyA3        = "payment method must contain currency code a3 for production settings"
+	paymentMethodErrorIncorrectTerminal          = "payment method must contain terminal settings for production settings"
+	paymentMethodErrorEmptyPassword              = "payment method must contain password for production settings"
+	paymentMethodErrorEmptyCallbackPassword      = "payment method must contain callback password for production settings"
+	paymentMethodErrorUnknownMethod              = "payment method is unknown"
+	paymentMethodErrorNotFoundProductionSettings = "payment method is not contain requesting settings"
 )
 
 func (s *Service) CreateOrUpdatePaymentMethod(
@@ -64,6 +70,108 @@ func (s *Service) CreateOrUpdatePaymentMethod(
 	}
 
 	if err != nil {
+		rsp.Status = pkg.ResponseStatusSystemError
+		rsp.Message = err.Error()
+
+		return nil
+	}
+
+	return nil
+}
+
+func (s *Service) CreateOrUpdatePaymentMethodProductionSettings(
+	ctx context.Context,
+	req *grpc.ChangePaymentMethodRequest,
+	rsp *grpc.ChangePaymentMethodParamsResponse,
+) error {
+	var pm *billing.PaymentMethod
+	var err error
+
+	pm, err = s.paymentMethod.GetById(req.PaymentMethodId)
+	if err != nil {
+		rsp.Status = pkg.ResponseStatusNotFound
+		rsp.Message = paymentMethodErrorUnknownMethod
+
+		return nil
+	}
+
+	if req.Params.CurrencyCodeA3 == "" || len(req.Params.CurrencyCodeA3) != 2 {
+		rsp.Status = pkg.ResponseStatusBadData
+		rsp.Message = paymentMethodErrorIncorrectCurrencyA3
+
+		return nil
+	}
+
+	if req.Params.Password == "" {
+		rsp.Status = pkg.ResponseStatusBadData
+		rsp.Message = paymentMethodErrorEmptyPassword
+
+		return nil
+	}
+
+	if req.Params.CallbackPassword == "" {
+		rsp.Status = pkg.ResponseStatusBadData
+		rsp.Message = paymentMethodErrorEmptyCallbackPassword
+
+		return nil
+	}
+
+	if req.Params.Terminal == "" {
+		rsp.Status = pkg.ResponseStatusBadData
+		rsp.Message = paymentMethodErrorIncorrectTerminal
+
+		return nil
+	}
+
+	pm.ProductionSettings[req.Params.CurrencyCodeA3] = req.Params
+	if err := s.paymentMethod.Update(pm); err != nil {
+		rsp.Status = pkg.ResponseStatusSystemError
+		rsp.Message = err.Error()
+
+		return nil
+	}
+
+	return nil
+}
+
+func (s *Service) GetPaymentMethodProductionSettings(
+	ctx context.Context,
+	req *grpc.GetPaymentMethodProductionSettingsRequest,
+	rsp *billing.PaymentMethodParams,
+) error {
+	pm, err := s.paymentMethod.GetById(req.PaymentMethodId)
+	if err != nil {
+		return nil
+	}
+
+	rsp, _ = pm.ProductionSettings[req.CurrencyA3]
+
+	return nil
+}
+
+func (s *Service) DeletePaymentMethodProductionSettings(
+	ctx context.Context,
+	req *grpc.GetPaymentMethodProductionSettingsRequest,
+	rsp *grpc.ChangePaymentMethodParamsResponse,
+) error {
+	pm, err := s.paymentMethod.GetById(req.PaymentMethodId)
+	if err != nil {
+		rsp.Status = pkg.ResponseStatusNotFound
+		rsp.Message = paymentMethodErrorUnknownMethod
+
+		return nil
+	}
+
+	if _, ok := pm.ProductionSettings[req.CurrencyA3]; !ok {
+		rsp.Status = pkg.ResponseStatusNotFound
+		rsp.Message = paymentMethodErrorNotFoundProductionSettings
+
+		return nil
+	}
+
+	delete(pm.ProductionSettings, req.CurrencyA3)
+
+	if err := s.paymentMethod.Update(pm); err != nil {
 		rsp.Status = pkg.ResponseStatusSystemError
 		rsp.Message = err.Error()
 
