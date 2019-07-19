f48393a11b9b33cd9cf3cfd6ee01663485ad4780
EvgeniyStrigo <evgeniy.strigo@protocol.one>
2019-07-19 17:21:26 +0300

    PAY-260 fixes

--- internal/app.go
+++ internal/app.go
@@ -16,6 +16,7 @@ import (
 	"github.com/micro/go-plugins/selector/static"
 	"github.com/paysuper/paysuper-billing-server/internal/config"
 	"github.com/paysuper/paysuper-billing-server/internal/database"
+	"github.com/paysuper/paysuper-billing-server/internal/mock"
 	"github.com/paysuper/paysuper-billing-server/internal/service"
 	"github.com/paysuper/paysuper-billing-server/pkg"
 	"github.com/paysuper/paysuper-billing-server/pkg/proto/grpc"
@@ -125,13 +126,15 @@ func (app *Application) Init() {
 	repService := repository.NewRepositoryService(constant.PayOneRepositoryServiceName, app.service.Client())
 	taxService := tax_service.NewTaxService(taxPkg.ServiceName, app.service.Client())
 
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
+	/*redisdb := redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs:        cfg.CacheRedis.Address,
 		Password:     cfg.CacheRedis.Password,
 		MaxRetries:   cfg.CacheRedis.MaxRetries,
 		MaxRedirects: cfg.CacheRedis.MaxRedirects,
 		PoolSize:     cfg.CacheRedis.PoolSize,
-	})
+	})*/
+
+	redisdb := mock.NewTestRedis()
 
 	app.svc = service.NewBillingService(
 		app.database,
