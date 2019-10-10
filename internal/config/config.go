3d70b36cfa4fbe5661f31f6376fe5b8db3d853e3
Vadim Sabirov <pr0head@gmail.com>
2019-10-10 14:37:10 +0300

    #192213 Send email after invitation

--- internal/config/config.go
+++ internal/config/config.go
@@ -57,6 +57,7 @@ type Config struct {
 	EmailGameCodeTemplate            string `envconfig:"EMAIL_ACTIVATION_CODE_TEMPLATE" default:"p1_verify_letter-2"`
 	EmailSuccessTransactionTemplate  string `envconfig:"EMAIL_SUCCESS_TRANSACTION_TEMPLATE" default:"p1_verify_letter-4"`
 	EmailRefundTransactionTemplate   string `envconfig:"EMAIL_REFUND_TRANSACTION_TEMPLATE" default:"p1_verify_letter-5"`
+	EmailInviteTemplate              string `envconfig:"EMAIL_INVITE_TEMPLATE" default:"code-your-own"`
 
 	RoyaltyReportsUrl string `envconfig:"ROYALTY_REPORTS_URL" default:"https://paysupermgmt.tst.protocol.one/royalty_reports"`
 	PayoutsUrl        string `envconfig:"PAYOUTS_URL" default:"https://paysupermgmt.tst.protocol.one/payout_documents"`
