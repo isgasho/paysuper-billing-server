fc8321f3ef84ff94b54a7c6ff80de986ac05c067
Vadim Sabirov <pr0head@gmail.com>
2019-10-15 19:20:25 +0300

    #192213 Init casbin on NewBillingService

--- internal/app.go
+++ internal/app.go
@@ -19,6 +19,7 @@ import (
 	"github.com/micro/go-micro/config/source"
 	goConfigCli "github.com/micro/go-micro/config/source/cli"
 	"github.com/micro/go-plugins/client/selector/static"
+	casbinProto "github.com/paysuper/casbin-server/internal/generated/api/proto/casbinpb"
 	documentSignerConst "github.com/paysuper/document-signer/pkg/constant"
 	documentSignerProto "github.com/paysuper/document-signer/pkg/proto"
 	"github.com/paysuper/paysuper-billing-server/internal/config"
@@ -179,6 +180,7 @@ func (app *Application) Init() {
 	curService := currencies.NewCurrencyratesService(curPkg.ServiceName, app.service.Client())
 	documentSignerService := documentSignerProto.NewDocumentSignerService(documentSignerConst.ServiceName, app.service.Client())
 	reporter := reporterService.NewReporterService(reporterServiceConst.ServiceName, app.service.Client())
+	casbin := casbinProto.NewCasbinService("p1casbin", app.service.Client())
 
 	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs:        cfg.CacheRedis.Address,
@@ -208,6 +210,7 @@ func (app *Application) Init() {
 		reporter,
 		formatter,
 		postmarkBroker,
+		casbin,
 	)
 
 	if err := app.svc.Init(); err != nil {
