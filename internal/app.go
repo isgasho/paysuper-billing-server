119311617578815e5380b59c2e7770a0e2638f91
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-11-04 12:45:07 +0300

    PAY-0 fix mongodb connection mode

--- internal/app.go
+++ internal/app.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ProtocolONE/geoip-service/pkg"
 	"github.com/ProtocolONE/geoip-service/pkg/proto"
 	metrics "github.com/ProtocolONE/go-micro-plugins/wrapper/monitoring/prometheus"
+	"github.com/globalsign/mgo"
 	"github.com/go-redis/redis"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
@@ -93,7 +94,8 @@ func (app *Application) Init() {
 
 	app.logger.Info("db migrations applied")
 
-	db, err := mongodb.NewDatabase()
+	opts := []mongodb.Option{mongodb.Mode(mgo.Primary)}
+	db, err := mongodb.NewDatabase(opts...)
 	if err != nil {
 		app.logger.Fatal("Database connection failed", zap.Error(err))
 	}
